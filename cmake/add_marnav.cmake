5bc78f110c6d32c911c9804c66d94c83ff2d1eab
Guillaume Vincke <guillaume.vincke@dice-engineering.com>
2022-10-11 18:20:54 +0200

    Add a patch on marnav to avoid using std::stod and use stringstream instead (compatibility with Qt)

--- cmake/add_marnav.cmake
+++ cmake/add_marnav.cmake
@@ -1,8 +1,8 @@
 include(FetchContent)
 
 FetchContent_Declare(marnav
-  GIT_REPOSITORY https://github.com/mariokonrad/marnav.git
-  GIT_TAG v0.13.0
+  URL https://github.com/mariokonrad/marnav/archive/refs/tags/v0.13.0.zip
+  PATCH_COMMAND git init && git apply --verbose --ignore-whitespace "${PROJECT_SOURCE_DIR}/cmake/patches/marnav.patch"
 )
 
 FetchContent_GetProperties(marnav)
@@ -13,7 +13,7 @@ if(NOT marnav_POPULATED)
   set(ENABLE_TESTS OFF CACHE BOOL "" FORCE)
   set(ENABLE_TESTS_BENCHMARK OFF CACHE BOOL "" FORCE)
   set(ENABLE_TOOLS OFF CACHE BOOL "" FORCE)
-  
+
   add_subdirectory(${marnav_SOURCE_DIR} ${marnav_BINARY_DIR})
 endif()
 
