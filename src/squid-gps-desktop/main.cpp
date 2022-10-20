5da438deb827440a5b051c632313a5807abf89e0
Guillaume Vincke <guillaume.vincke@dice-engineering.com>
2022-10-20 14:06:21 +0200

    Add CoInitialize call on windows

--- src/squid-gps-desktop/main.cpp
+++ src/squid-gps-desktop/main.cpp
@@ -2,7 +2,18 @@
 #include <QQmlApplicationEngine>
 #include <spdlog/spdlog.h>
 
+// Asio needs a call to CoInitialize on windows
+#ifdef CO_INITIALIZE
+#include <windows.h>
+void Initialize() {
+  CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+}
+#else  // CO_INITIALIZE
+void Initialize() {}
+#endif  // CO_INITIALIZE
+
 int main(int argc, char** argv) {
+  Initialize();
   spdlog::set_level(spdlog::level::info);
   QApplication app(argc, argv);
   QQmlApplicationEngine engine;
